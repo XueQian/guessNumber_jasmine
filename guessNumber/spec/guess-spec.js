4530a7e971d231c185e776d9eeed05864499fe99
XueQian <xueqian0321@126.com>
2014-08-22 12:38:59 +0800

    guess-spec finished

--- guessNumber/spec/guess-spec.js
+++ guessNumber/spec/guess-spec.js
@@ -1,15 +1,34 @@
-describe('guess', function () {
+describe('guess_print', function () {
     beforeEach(function () {
 
     });
-    it('guess OK', function () {
+    it('guess print is OK', function () {
         var inputs = '1234';
         var generateRand = new GenerateRand();
-        spyOn(generateRand,"rand").and.returnValue('4321');
+        spyOn(generateRand, "rand").and.returnValue('4321');
 
         var compareNumber = new CompareNumber();
-        var guesses = new Guess(generateRand,compareNumber);
-        var guessExpect = guesses.guess(inputs);
-        expect(guessExpect).toBe("0A4B");
+        spyOn(compareNumber, "compare").and.returnValue("0A4B");
+
+        var guesses = new Guess(generateRand, compareNumber);
+        var tips = guesses.guess(inputs);
+        expect(tips).toBe("0A4B");
     });
-})
+});
+
+describe("guess_spy on Math.rand()", function () {
+
+    beforeEach(function () {
+
+    });
+    it("should print 4A0B", function () {
+        var randoms = [0.1, 0.2, 0.3, 0.4];
+        spyOn(Math, 'random').and.callFake(function () {
+            return randoms.shift();
+        });
+        var inputs = "1234";
+        var guessExpect = new Guess(new GenerateRand(), new CompareNumber());
+        expect(guessExpect.guess(inputs)).toEqual("4A0B");
+    });
+});
+
